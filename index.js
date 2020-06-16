bc03f93c403bbb64b3be5f6b3e5d7c156e79612e
Carlos Henrique <carlos_henrique@acaciaconsultoria.com.br>
2020-06-15 21:27:14 -0300

    Exercício 3 - Objetivo: Crie dois métodos no objeto "database" chamados de "createTable" e "execute". O comando "createTable" já foi implementado no exercício anterior, mova o código e utilize o método "execute" para invocar dinamicamente o método "createTable".

--- index.js
+++ index.js
@@ -1,34 +1,46 @@
+const database = Object.assign(
+  {},
+  {
+    tables: {},
+    createTable: function (sqlCommand) {
+      const regExp = /^create\s+table\s+([\w\S]+)\s*\(([\W\S]+)\)$/;
+      const result = sqlCommand.match(regExp);
+
+      const tableName = result && result.length > 1 ? result[1] : null;
+      const tableColumns =
+        result && result.length > 2 ? result[2].split(",") : null;
+
+      if (!tableName || !tableColumns) return;
+
+      this.tables[tableName] = {
+        columns: {},
+        data: [],
+      };
+
+      for (let key in tableColumns) {
+        const columnProperty = tableColumns[key].trim().split(" ");
+        if (!columnProperty || columnProperty.length < 2) continue;
+
+        const columnName = columnProperty[0];
+        const columnType = columnProperty[1];
+        if (!columnName || !columnType) continue;
+
+        this.tables[tableName].columns[columnName] = columnType;
+      }
+    },
+    execute: function (sqlCommand) {
+      if (!sqlCommand) return;
+
+      const cmd = sqlCommand.toLowerCase();
+      if (cmd.startsWith("create table ")) this.createTable(sqlCommand);
+    },
+  }
+);
+
 const sqlCommand =
   "create table author (id number, name string, age number,\
   city string, state string, country string)";
 
-const regExp = /^create\s+table\s+([\w\S]+)\s*\(([\W\S]+)\)$/;
-const result = sqlCommand.match(regExp);
+database.execute(sqlCommand);
 
-const tableName = result && result.length > 1 ? result[1] : null;
-const tableColumns = result && result.length > 2 ? result[2].split(",") : null;
-
-if (!tableName || !tableColumns) return;
-
-const database = Object.assign(
-  {},
-  {
-    tables: {
-      [tableName]: {
-        columns: {},
-      },
-      data: [],
-    },
-  }
-);
-for (let key in tableColumns) {
-  const columnProperty = tableColumns[key].trim().split(" ");
-  if (!columnProperty || columnProperty.length < 2) continue;
-
-  const columnName = columnProperty[0];
-  const columnType = columnProperty[1];
-  if (!columnName || !columnType) continue;
-
-  database.tables[tableName].columns[columnName] = columnType;
-}
 console.log(JSON.stringify(database, null, "    "));
