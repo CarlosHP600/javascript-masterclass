9cdb3c0fc632756e633d0444702adb12abf3ff3b
Carlos Henrique <carlos_henrique@acaciaconsultoria.com.br>
2020-06-15 21:25:38 -0300

    Exercício 2 - Objetivo: Com base no nome da tabela e nas colunas, monte uma estrutura de objetos para armazenar tanto a definição da tabela quanto os dados.

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-let sqlCommand = "create table author (id number, name string, age number,\
+const sqlCommand =
+  "create table author (id number, name string, age number,\
   city string, state string, country string)";
 
-let regExp = /^create\s+table\s+([\w\S]+)\s*\(([\W\S]+)\)$/
-let result = sqlCommand.match(regExp);
+const regExp = /^create\s+table\s+([\w\S]+)\s*\(([\W\S]+)\)$/;
+const result = sqlCommand.match(regExp);
 
-let tableName = result && result.length > 1? result[1] : null; 
-let columnsArray = result && result.length > 2? result[2].split(",") : null;
+const tableName = result && result.length > 1 ? result[1] : null;
+const tableColumns = result && result.length > 2 ? result[2].split(",") : null;
 
-console.log(`tableName -> ${(tableName) ? tableName : 
- "Nome da tabela não localizado!" }`);
+if (!tableName || !tableColumns) return;
 
-console.log(`columns -> ${ (columnsArray) ? columnsArray : 
- "Não foi possivel definir os campos!"}`);
+const database = Object.assign(
+  {},
+  {
+    tables: {
+      [tableName]: {
+        columns: {},
+      },
+      data: [],
+    },
+  }
+);
+for (let key in tableColumns) {
+  const columnProperty = tableColumns[key].trim().split(" ");
+  if (!columnProperty || columnProperty.length < 2) continue;
+
+  const columnName = columnProperty[0];
+  const columnType = columnProperty[1];
+  if (!columnName || !columnType) continue;
+
+  database.tables[tableName].columns[columnName] = columnType;
+}
+console.log(JSON.stringify(database, null, "    "));
