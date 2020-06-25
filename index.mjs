57104ad56bbad6ce1fd41562fd281188a8ba6ace
Carlos Henrique <carlos_henrique@acaciaconsultoria.com.br>
2020-06-24 21:38:03 -0300

    Exercício 11 - Objetivo: Crie um atraso no retorno da função execute por meio de setTimeout e utilize uma promise para tratar o comportamento assíncrono.

--- index.mjs
+++ index.mjs
@@ -1,25 +1,31 @@
 import DataBase from "./database.mjs";
 
-const database = new DataBase();
-try {
-  const createCommand =
-    "create table author (id number, name string, age number, city string,\
+const createCommand =
+  "create table author (id number, name string, age number, city string,\
        state string, country string)";
-  database.execute(createCommand);
 
-  const insertCommands = [
-    "insert into author (id, name, age) values (1, Douglas Crockford, 62)",
-    "insert into author (id, name, age) values (2, Linus Torvalds, 47)",
-    "insert into author (id, name, age) values (3, Martin Fowler, 54)",
-  ];
-  for (const insertCommand of insertCommands) {
-    database.execute(insertCommand);
-  }
-  const deleteComand = "delete from author where id = 2";
-  database.execute(deleteComand);
+const insertCommands = [
+  "insert into author (id, name, age) values (1, Douglas Crockford, 62)",
+  "insert into author (id, name, age) values (2, Linus Torvalds, 47)",
+  "insert into author (id, name, age) values (3, Martin Fowler, 54)",
+];
 
-  const selectCommand = "select name, age from author";
-  console.log(JSON.stringify(database.execute(selectCommand), undefined, " "));
-} catch (e) {
-  console.log(e.message);
-}
+const selectCommand = "select name, age from author";
+
+const database = new DataBase();
+
+database.execute(createCommand)
+  .then(() => {
+    return Promise.all([
+      database.execute(insertCommands[0]),
+      database.execute(insertCommands[1]),
+      database.execute(insertCommands[2]),
+    ]).then(() => {
+      return database.execute(selectCommand).then((result) => {
+        console.log(JSON.stringify(result, undefined, " "));
+      });
+    });
+  })
+  .catch((e) => {
+    console.log(e.message);
+  });
